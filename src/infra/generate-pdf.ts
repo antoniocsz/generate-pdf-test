ce0e1e7e88f5be6cc3929a96e5c8956c5fc4ab98
Antonio Santana <antcesar.santana@gmail.com>
2024-05-20 23:00:02 -0300

    add args no-sandbox

--- src/infra/generate-pdf.ts
+++ src/infra/generate-pdf.ts
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer'
 
 export async function createPDF({ content }: { content: any}): Promise<Buffer> {
-    const browser = await puppeteer.launch({ headless: 'shell' })
+    const browser = await puppeteer.launch({ headless: 'shell', args: ['--no-sandbox'] })
     const page = await browser.newPage()
     await page.setContent(content)
     const pdfBuffer = await page.pdf({ format: 'A4' })
