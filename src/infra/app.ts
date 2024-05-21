65a879db13edd9c26800a995c0f66945384eeb35
Antonio Santana <antcesar.santana@gmail.com>
2024-05-20 22:18:53 -0300

    add teste

---
+++ src/infra/app.ts
@@ -0,0 +1,50 @@
+import fastify from 'fastify'
+import { createPDF } from './generate-pdf'
+
+export const app = fastify()
+
+app.post('/api/generate-pdf',async (request, reply) => {
+  const content = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Relatório de Movimentações</title>
+      <style>
+        body { font-family: Arial, sans-serif; padding: 32px; font-size: 14px; }
+        table { width: 100%; border-collapse: collapse; }
+        th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }
+        th { background-color: #f2f2f2; }
+        h1 { font-size: 32px; }
+        h2 { font-size: 16px; }
+        section { padding: 64px; background-color: rgb(71, 153, 235); }
+      </style>
+    </head>
+
+    <body>
+      <div style="margin-bottom: 24px; display: flex; flex-direction: column; gap: 4px;">   
+      </div>
+
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Categoria</th>
+          <th>Tipo</th>
+          <th>Data</th>
+          <th>Status</th>
+          <th>Valor</th>
+        </tr>
+      </table>
+    </body>
+  </html>
+  `;
+  
+  const file = await  createPDF({ content })
+
+  return reply
+    .header('Content-Type', 'application/octet-stream')
+    .header(
+      'Content-Disposition',
+      `attachment; filename="file.pdf"`,
+    ).type(`application/pdf`)
+    .send(file)
+});
---
+++ src/infra/generate-pdf.ts
@@ -0,0 +1,10 @@
+import puppeteer from 'puppeteer'
+
+export async function createPDF({ content }: { content: any}): Promise<Buffer> {
+    const browser = await puppeteer.launch({ headless: 'shell' })
+    const page = await browser.newPage()
+    await page.setContent(content)
+    const pdfBuffer = await page.pdf({ format: 'A4' })
+    await browser.close()
+    return pdfBuffer
+}
